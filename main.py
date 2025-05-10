2303fedb1f1426a7ffbf3e51dc88edad256452e2
JeanPaulCP <140226335+JeanPaulCP@users.noreply.github.com>
2025-05-10 16:52:16 -0500

    Actualizo las credenciales de la base de datos MySQL

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 from .routers import usuarios
 from . import models, database
+from fastapi.middleware.cors import CORSMiddleware
 
+# Crear tablas si no existen
 models.Base.metadata.create_all(bind=database.engine)
 
+# Crear app
 app = FastAPI()
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes reemplazar "*" con ["http://localhost:3000"] en producción
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Registrar rutas
 app.include_router(usuarios.router)
